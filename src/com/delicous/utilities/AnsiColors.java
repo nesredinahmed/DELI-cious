e248d5a23139376524b5d63b3d735f31091c0880
Nesredin Ahmed <ahmednesredin@icloud.com>
2025-05-29 22:30:28 -0700

    Updating

--- src/com/delicous/utilities/AnsiColors.java
+++ src/com/delicous/utilities/AnsiColors.java
@@ -5,7 +5,6 @@ package com.delicous.utilities;
  */
 public class AnsiColors {
     public static final String RESET = "\u001B[0m";
-    public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
@@ -16,29 +15,14 @@ public class AnsiColors {
 
     // Bright colors
     public static final String BRIGHT_BLACK = "\u001B[90m";
-    public static final String BRIGHT_RED = "\u001B[91m";
     public static final String BRIGHT_GREEN = "\u001B[92m";
     public static final String BRIGHT_YELLOW = "\u001B[93m";
     public static final String BRIGHT_BLUE = "\u001B[94m";
-    public static final String BRIGHT_PURPLE = "\u001B[95m";
     public static final String BRIGHT_CYAN = "\u001B[96m";
     public static final String BRIGHT_WHITE = "\u001B[97m";
 
-    // Background colors
-    public static final String BG_BLACK = "\u001B[40m";
-    public static final String BG_RED = "\u001B[41m";
-    public static final String BG_GREEN = "\u001B[42m";
-    public static final String BG_YELLOW = "\u001B[43m";
-    public static final String BG_BLUE = "\u001B[44m";
-    public static final String BG_PURPLE = "\u001B[45m";
-    public static final String BG_CYAN = "\u001B[46m";
-    public static final String BG_WHITE = "\u001B[47m";
 
     // Text styles
     public static final String BOLD = "\u001B[1m";
-    public static final String UNDERLINE = "\u001B[4m";
-    public static final String BLINK = "\u001B[5m";
-    public static final String REVERSE = "\u001B[7m";
-    public static final String CONCEAL = "\u001B[8m";
-    public static final String CROSSED_OUT = "\u001B[9m";
+
 }
